2c326aace29873975dc3144bed2065e3b45cdd6a
alicanselen <81762458+alicanselen@users.noreply.github.com>
2024-09-24 17:09:18 +0300

    Etkinlik Detayi Sayfasi Dizayn Edildi

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,11 +1,29 @@
-import { useLocalSearchParams } from "expo-router";
-import { Text } from "react-native";
+import { useLocalSearchParams , Stack} from "expo-router";
+import { Text, View , Image } from "react-native";
+import dayjs from "dayjs"
 import events from '~/assets/event.json'
+
 
 
 export default function EventPage(){
     const  {id } = useLocalSearchParams();
 
     const  event =events.find((e)=>e.id == id);
-    return <Text>Event Adi : {event.title}</Text>
+
+    if(!event){
+        return <Text>Etkinlik Bulunamadi</Text>
+    }
+    return (
+        <View className="p-3 bg-white gap-3">
+            <Stack.Screen options={{title :'Etkinlik Detayi' , headerBackTitleVisible: false , headerTintColor:'black'}}/>
+            <Image source={{uri:event.image}} className='w-full aspect-video rounded-xl'/>
+            <Text className='text-3xl font-bold' >{event.title}</Text>
+            <Text className='text-lg font semi-bold uppercase text-amber-800'>
+            {dayjs(event.datetime).format('ddd,D MMM')} . {dayjs(event.datetime).format('hh:mm A')} 
+            </Text>
+            <Text className='text-lg' >
+                {event.description}
+            </Text>
+        </View>
+    )
 }
