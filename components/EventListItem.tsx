1e51421b51d120921a7871ebadb5f125f9630b66
alicanselen <81762458+alicanselen@users.noreply.github.com>
2024-09-24 16:28:02 +0300

    Saat Tarih Parsing Tamamlandi

--- components/EventListItem.tsx
+++ components/EventListItem.tsx
@@ -1,15 +1,16 @@
 import Feather from '@expo/vector-icons/Feather';
 import { View , Text,Image } from 'react-native';
+import  dayjs from 'dayjs'
 
 
 
 export default function EventListItem({event}){
     return(
-        <View className='p-3 gap-3'>
+        <View className='p-3 gap-3 border-b-2 border-gray-100 pb-3'>
         <View className='flex-row'>
           <View className='flex-1 gap-2'>
         <Text className='text-lg font semi-bold uppercase text-amber-800'>
-            Wed 13 , Sep 
+            {dayjs(event.datetime).format('ddd,D MMM')} . {dayjs(event.datetime).format('hh:mm')} 
             </Text>
         <Text className='text-xl font-bold' >
             {event.title}
