90f381ed5488041b17d5743d110ed9b28a303a36
alicanselen <81762458+alicanselen@users.noreply.github.com>
2024-09-24 17:56:52 +0300

    Etkinlik Detayi Sayfasi Join Footer

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,5 +1,5 @@
 import { useLocalSearchParams , Stack} from "expo-router";
-import { Text, View , Image } from "react-native";
+import { Text, View , Image, Pressable } from "react-native";
 import dayjs from "dayjs"
 import events from '~/assets/event.json'
 
@@ -14,7 +14,7 @@ export default function EventPage(){
         return <Text>Etkinlik Bulunamadi</Text>
     }
     return (
-        <View className="p-3 bg-white gap-3">
+        <View className="flex-1 p-3 bg-white gap-3">
             <Stack.Screen options={{title :'Etkinlik Detayi' , headerBackTitleVisible: false , headerTintColor:'black'}}/>
             <Image source={{uri:event.image}} className='w-full aspect-video rounded-xl'/>
             <Text className='text-3xl font-bold' >{event.title}</Text>
@@ -24,6 +24,16 @@ export default function EventPage(){
             <Text className='text-lg' >
                 {event.description}
             </Text>
+
+            {/* Footer */}
+
+            <View className="absolute bottom-0 left-0  right-0 flex-row justify-between border-t-2 border-gray-400 p-5 pb-10 items-center">
+            <Text className="text-xl font-semibold">Free</Text>
+                
+                <Pressable className="bg-red-500 p-5 px-8 rounded-md">
+                    <Text className="text-lg font-bold text-white">Join and Rsvp</Text>
+                </Pressable>
+            </View>
         </View>
     )
 }
