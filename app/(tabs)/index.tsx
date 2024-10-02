257e4f0641f164c359bc3751fe30efea15fcd023
alicanselen <81762458+alicanselen@users.noreply.github.com>
2024-10-02 15:44:56 +0300

    Kullanicinin konum bilgilerini alarak kac km uzakt oldugunu gosterecek sekilde ayarlamalar yapildi

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,36 @@
-import { Stack } from 'expo-router';;
-import { View , Text,Image , FlatList } from 'react-native';
+import { Stack } from 'expo-router';
+import * as Location from 'expo-location';
+import { View , Text,Image , FlatList, Alert } from 'react-native';
 import EventListItem from '~/components/EventListItem';
 import { supabase } from '~/utils/supabase';
 import { useEffect, useState } from 'react';
 import { NearbyEvent } from '~/types/db';
+
 export default function Events() {
+
+  const [location, setLocation] = useState<Location.LocationObject |null>(null);
   const [events , setEvents] = useState<NearbyEvent[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
 
 
   useEffect(()=> {
-    fetchNearbyEvents();
-  },[]);
+    if(location){
+      fetchNearbyEvents();
+    }
+  },[location]);
 
 
   const fetchAllEvents = async ()=>{
@@ -19,9 +39,12 @@ export default function Events() {
   };
 
   const fetchNearbyEvents = async ()=>{
+    if(!location){
+      return;
+    }
     const {data , error }  = await supabase.rpc('nearby_events' ,  {
-      lat:41.137978, 
-      long:29.053537,
+      lat:location.coords.latitude, 
+      long:location.coords.longitude,
     });
     console.log(data);
     console.log(error);
